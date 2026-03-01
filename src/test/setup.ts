c00aae6c2a3ea536a0c55b5785704afe66d1d2c3
Bash0121 <Robert.smith@students.fhu.edu>
2026-02-28 18:42:24 -0600

    added prisma tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom";
 import { TextEncoder, TextDecoder } from "util";
+import { beforeEach, afterEach, vi } from "vitest";
 Object.assign(globalThis, { TextEncoder, TextDecoder });
 
 beforeEach(() => {
